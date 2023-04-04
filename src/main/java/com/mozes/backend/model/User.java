8f67918b09890dde5cbb0394313d4d0162d2d91d
mozes20 <htcmokka@gmail.com>
2023-04-04 15:17:04 +0200

    Added User entity

---
+++ src/main/java/com/mozes/backend/model/User.java
@@ -0,0 +1,71 @@
+package com.mozes.backend.model;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private boolean active;
+    @Column
+    private String address;
+    @Column
+    private boolean created;
+    @Column
+    private Date created_at;
+    @Column
+    private boolean deleted;
+    @Column
+    private Date deleted_at;
+    @Column
+    private String delete_flag;
+    @Column
+    private String email;
+    @Column
+    private String email_token;
+    @Column
+    private Date last_login;
+    @Column
+    private String name;
+    @Column
+    private boolean next_login_change_pwd;
+    @Column
+    private String password;
+    @Column
+    private boolean password_expired;
+    @Column
+    private String phone;
+    @Column
+    private int settlement_id;
+    @Column
+    private String temp_password;
+    @Column
+    private boolean temp_password_expired;
+    @Column
+    private boolean updated;
+    @Column
+    private Date updated_at;
+    @Column
+    private String user_type;
+    @Column
+    private String username;
+    @Column
+    private int settlements_by_settlement_id;
+    @Column
+    private int user_by_created_id;
+    @Column
+    private int user_by_updated_id;
+    @Column
+    private int user_by_deleted_id;
+
+}
